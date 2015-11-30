48be5f305356c81546c3441e3175718cf9928d95
James Abel <j@abel.co>
2015-11-29 23:50:29 -0800

    include required packages

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from distutils.core import setup
 import py2exe
 
 setup(console=['mops.py'],
+      packages=['mops'],
+      options={"py2exe":
+          {
+              "includes": ['redis', 'win32api', 'win32con', 'requests', 'psutil' ]
+          }
+      }
 
       # single executable doesn't seem to work
       #options = {'py2exe': {'bundle_files': 1}},
