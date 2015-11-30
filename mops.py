e8d1d9f66c9e84442ab5abcb9b7c77d2bd63bc8e
James Abel <j@abel.co>
2015-11-29 21:54:51 -0800

    refactor into mops module

--- mops.py
+++ mops.py
@@ -1,47 +1,27 @@
 
 import argparse
-import os
-import configparser
 
-import mops.mops
+import mops.app
+import mops.gui
+import mops.config
 
 
 def main():
-    company = 'abel'
-    application = 'iwantmyrdp'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--advertise', help="advertise that this computer accepts RDP (usually execute on startup)")
-    parser.add_argument('-c', '--connect', help="connect to a computer")
-    parser.add_argument('-r', '--redis', nargs=2, help="set redis endpoint and password")
+    parser.add_argument('-r', '--redis', nargs=2, help="provide and set redis endpoint and password")
+    parser.add_argument('-t', '--test', action="store_true", help="use test data (don't access the redis DB)")
     parser.add_argument('-v', '--verbose', action="store_true", help='print verbose status messages')
     args = parser.parse_args()
 
-    config_file_folder = os.path.join(os.getenv('APPDATA'), company, application)
-    if not os.path.exists(config_file_folder):
-        os.makedirs(config_file_folder)
-    config_file_path = os.path.join(config_file_folder, 'config.ini')
-
+    config = mops.config.MopsConfig()
     if args.verbose:
-        print('config file path : %s' % config_file_path)
+        print('config file path: %s' % config.get_config_file_path())
 
-    did_something = False
+    if args.redis and not args.nodb:
+        config.set_redis_login(args.redis[0], args.redis[1])
 
-    if args.redis:
-        config = configparser.ConfigParser()
-        config['redis'] = {'endpoint': args.redis[0], 'password': args.redis[1]}
-        with open(config_file_path, 'w') as configfile:
-            config.write(configfile)
-        did_something = True
-
-    #rdp = iwantmyrdp.iwantmyrdp.IWantMyRDP()
-    #print(rdp.get_rdp_systems())
-
-    if not did_something:
-        print('nothing to do - execute with --help for help')
-
-    m = mops.mops.Mops()
-
+    mops.app.main(args.test, args.verbose)
 
 if __name__ == '__main__':
     main()
