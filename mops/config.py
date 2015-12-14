44bca9dceb741a9f0e90b698e2652e985dd44c60
James Abel <j@abel.co>
2015-12-14 11:06:23 -0800

    add config file param

--- mops/config.py
+++ mops/config.py
@@ -7,23 +7,63 @@ application = 'mops'
 
 
 class MopsConfig:
-    def __init__(self):
-        config_file_folder = os.path.join(os.getenv('APPDATA'), company, application)
-        if not os.path.exists(config_file_folder):
-            os.makedirs(config_file_folder)
-        self.config_file_path = os.path.join(config_file_folder, 'config.ini')
+    def __init__(self, config_folder_path = None):
+        if not config_folder_path:
+            config_folder_path = os.path.join(os.getenv('APPDATA'), company, application)
+        if not os.path.exists(config_folder_path):
+            os.makedirs(config_folder_path)
+        self._config_file_path = os.path.join(config_folder_path, 'config.ini')
+        self._config = configparser.ConfigParser()
+
+    def _write(self):
+        with open(self._config_file_path, 'w') as configfile:
+            self._config.write(configfile)
+
+    def clear(self):
+        self._config.clear()
+        self._write()
 
     def get_config_file_path(self):
-        return self.config_file_path
+        return self._config_file_path
 
     def set_redis_login(self, endpoint, password):
-        config = configparser.ConfigParser()
-        config['redis'] = {'endpoint': endpoint, 'password': password}
-        with open(self.config_file_path, 'w') as configfile:
-            config.write(configfile)
+        self._config['redis'] = {'endpoint': endpoint, 'password': password}
+        self._write()
 
     def get_redis_login(self):
-        config = configparser.ConfigParser()
-        config.read(self.config_file_path)
-        redis_config = config['redis']
-        return redis_config['endpoint'], redis_config['password']
+        endpoint = None
+        password = None
+        if 'redis' in self._config:
+            redis = self._config['redis']
+            if 'endpoint' in redis:
+                endpoint = redis['endpoint']
+            if 'password' in redis:
+                password = redis['password']
+        return endpoint, password
+
+    def set_run_on_windows_startup(self, state):
+        self._config['preferences'] = {'runonstartup': state}
+        self._write()
+
+    def get_run_on_windows_startup(self):
+        state = None
+        if 'preferences' in self._config:
+            preferences = self._config['preferences']
+            if 'runonstartup' in preferences:
+                state = preferences['runonstartup']
+        return state
+
+
+def _test():
+    c = MopsConfig('temp')
+    print(os.path.abspath(c.get_config_file_path()))
+    c.clear()
+    print(c.get_redis_login())
+    print(c.get_run_on_windows_startup())
+    c.set_redis_login('my_endpoint', 'my_password')
+    c.set_run_on_windows_startup(True)
+    print(c.get_redis_login())
+    print(c.get_run_on_windows_startup())
+
+if __name__ == '__main__':
+    _test()
