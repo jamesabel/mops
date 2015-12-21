04daad2a891fb2f346c537c4f3136b8ebd0745a9
James Abel <j@abel.co>
2015-12-20 21:08:42 -0800

    remove what are now extraneous command line options

--- mops.py
+++ mops.py
@@ -11,21 +11,9 @@ def main():
     mops.logger.init()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--redis', nargs=2, help="provide and set redis endpoint and password")
     parser.add_argument('-t', '--test', action="store_true", help="use test data (don't access the redis DB)")
-    parser.add_argument('-c', '--clear', action="store_true", help="clear preferences")
     parser.add_argument('-v', '--verbose', action="store_true", help='print verbose status messages')
     args = parser.parse_args()
-
-    config = mops.preferences.MopsPreferences()
-
-    mops.logger.log.info('config file path: %s' % config.get_preferences_file_path())
-
-    if args.clear:
-        config.clear()
-
-    if args.redis and not args.test:
-        config.set_redis_login(args.redis[0], args.redis[1])
 
     app = mops.app.App(args.test, args.verbose)
     app.exec()
