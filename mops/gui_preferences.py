051a809cac2a511f3db6181a1ca8fb16727cd8ed
James Abel <j@abel.co>
2015-12-24 11:19:42 -0800

    add redis labs comment

--- mops/gui_preferences.py
+++ mops/gui_preferences.py
@@ -24,17 +24,23 @@ class GUIPreferences(QDialog):
         self.endpoint_line_edit.setMinimumWidth(max_width)
         self.password_line_edit = QLineEdit(password)
         self.password_line_edit.setMinimumWidth(max_width)
-        layout.addWidget(QLabel('redis endpoint'), 0, 0)
+        layout.addWidget(QLabel('redis endpoint:', alignment=Qt.AlignRight), 0, 0)
         layout.addWidget(self.endpoint_line_edit, 0, 1)
-        layout.addWidget(QLabel('redis password'), 1, 0)
+        layout.addWidget(QLabel('redis password:', alignment=Qt.AlignRight), 1, 0)
         layout.addWidget(self.password_line_edit, 1, 1)
+
+        layout.addWidget(QLabel(''), 2, 0)
+        layout.addWidget(QLabel("If you don't already have a redis database you can get one free at:"), 3, 1)
+        get_redis_le = QLineEdit("www.redislabs.com")
+        get_redis_le.setReadOnly(True)
+        layout.addWidget(get_redis_le, 4, 1)
 
         buttons = QDialogButtonBox(
             QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
             Qt.Horizontal, self)
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
-        layout.addWidget(buttons)
+        layout.addWidget(buttons, 5, 1)
 
         self.show()
 
