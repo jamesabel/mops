781929561df60b202d29952eedf6054cd2d57466
James Abel <j@abel.co>
2015-12-26 23:38:07 -0800

    add logger

--- test/test_kv.py
+++ test/test_kv.py
@@ -1,8 +1,13 @@
 
 import mops.system_metrics
 import mops.db
+import mops.logger
+
 
 def test_kv():
+    temp_folder = 'temp'
+    mops.logger.init(temp_folder)
+
     m = mops.system_metrics.get_metrics()
     kv = mops.db.dict_to_kv(m)
     m2 = mops.db.kv_to_dict(kv)
