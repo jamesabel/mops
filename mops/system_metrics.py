33cb116e359aa57251133c1e5e59b7f870febc7e
James Abel <j@abel.co>
2015-12-26 23:39:44 -0800

    deal with disks with no volume label

--- mops/system_metrics.py
+++ mops/system_metrics.py
@@ -2,10 +2,11 @@
 import socket
 import datetime
 import platform
-import collections
 import win32api
 import psutil
 import uptime
+
+import mops.logger
 
 """
 gather system metrics/info
@@ -26,19 +27,32 @@ def get_metrics():
     metrics['uptime'] = str(datetime.timedelta(seconds=uptime.uptime()))
 
     metrics['disk'] = {}
-    for disk in psutil.disk_partitions(all=True):
+    disk_partitions = psutil.disk_partitions(all=True)
+    mops.logger.log.debug('disk partitions: %s' % str(disk_partitions))
+    for disk in disk_partitions:
         disk_path = disk[0]
+        volume_name = None
+        used = 0
+        total = 0
+        info_ok = True
         try:
             total, used, free, percent = psutil.disk_usage(disk_path)
+        except:
+            info_ok = False
+            mops.logger.log.debug('psutil.disk_usage(%s)' % str(disk_path))
+        try:
             volume_name, serial_number, max_len, flags, fs_name = win32api.GetVolumeInformation(disk_path)
         except:
-            volume_name = None
-            used = None
-            total = None
-        if volume_name:
+            info_ok = False
+            mops.logger.log.debug('win32api.GetVolumeInformation(%s)' % str(disk_path))
+
+        mops.logger.log.debug('disk_path: %s, volume_name: %s, used: %s, total: %s' %
+                              (disk_path, volume_name, str(used), str(total)))
+        if info_ok:
             disk_name = disk_path[0]
             metrics['disk'][disk_name] = {}
             metrics['disk'][disk_name]['volume'] = volume_name
             metrics['disk'][disk_name]['used'] = str(used)
             metrics['disk'][disk_name]['total'] = str(total)
+
     return {get_computer_name(): metrics}
