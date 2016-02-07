536c7e96acc7ae47772c56f28f2b73208ae94f89
James Abel <james.c.abel@intel.com>
2016-02-07 21:48:00 +0200

    add timestamp to test

--- test/test_gui_systems.py
+++ test/test_gui_systems.py
@@ -20,6 +20,7 @@ def test_gui_systems():
     collector.start()
     time.sleep(3)  # todo: use an event for this that the collector has collected everything
     m = collector.get_metrics()
+    m['timestamp'] = time.time()
     gui = mops.gui_systems.GUI(False)
     gui.run(m)
     collector.request_exit()
